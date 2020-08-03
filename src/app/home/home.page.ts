85849d9d5e0b6c145527da40deab1253f75e1bbf
Ken <kconstantej@est.ups.edu.ec>
2020-08-03 18:20:03 -0500

    configuracion_temporizador3

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
-import { isUndefined } from 'util';
 import { Storage } from '@ionic/storage';
 
 @Component({
@@ -40,43 +38,3 @@ export class HomePage {
   //hasta aqui
 
 }
-=======
-import { Component } from '@angular/core';
-import { isUndefined } from 'util';
-import { DataLocalService } from '../services/data-local.service';
-
-@Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-})
-export class HomePage {
-  temas={
-    imagen_fondo: '',
-   
-  };
-  
-  imagen_base:string;
-  
-  
-  constructor( private datalocal: DataLocalService ) {
-    this.datalocal.firstTime();
-    this.validar_fondo(localStorage.getItem('fondo'))
-    this.imagen_base= localStorage.getItem('fondo');
-    console.log('home',this.imagen_base)
-  }
-  validar_fondo(fondo){
-    
-    if(fondo===''){
-      this.imagen_base= 'backgroundClaro';
-      localStorage.setItem('fondo',this.imagen_base);
-      console.log("casaaaaaaaaaaaa",this.imagen_base);
-    }else{
-      this.imagen_base=localStorage.getItem('fondo');
-    
-      
-    }
-  }
-
-}
->>>>>>> 4a3ac8f7156fc096238b2d6c166e7e8927e25050
