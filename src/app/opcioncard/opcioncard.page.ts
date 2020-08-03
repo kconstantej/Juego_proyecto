979838c0da7bf8568bd834dc32fe75c4301f77f3
kevin <kevinchacha29@yahoo.com>
2020-08-03 12:07:16 -0500

    reloj configurado

--- src/app/opcioncard/opcioncard.page.ts
+++ src/app/opcioncard/opcioncard.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CardsDBService } from '../services/cards-db.service';
 import { Rojo } from '../interfaces/interfaces';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-opcioncard',
@@ -8,15 +9,39 @@ import { Rojo } from '../interfaces/interfaces';
   styleUrls: ['./opcioncard.page.scss'],
 })
 export class OpcioncardPage implements OnInit {
-
+  time :BehaviorSubject<string>=new BehaviorSubject('01:00');
+  timer: number;
   cards: Rojo [] = [];
   azarCard;
   resp: any [];
   title: string;
   ans;
-
   constructor( private cardsdb: CardsDBService ) { }
+  iniciar(duracion:number){
+    this.timer=duracion*60;
+    setInterval(()=>{
+      this.actualizartiempo();
+    },1000);
+  }
+  public disabled = false;
 
+  public action() {
+    this.disabled = !this.disabled;
+  }
+  actualizartiempo(){
+    let minutos :any=this.timer/60;
+    let segundos :any=this.timer % 60;
+    minutos =String('0'+Math.floor(minutos)).slice(-2);
+    segundos=String('0'+Math.floor(segundos)).slice(-2);
+
+    const text=minutos+":"+segundos;
+    this.time.next(text);
+    --this.timer;
+    if(this.timer<0){
+      this.iniciar(0);
+    }
+  }
+  
   ngOnInit() {
     this.cardsdb.getQuestion()
     .subscribe( resp => {
